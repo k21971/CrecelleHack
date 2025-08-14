4e5f8c9f5eace1e529aaea9084acf0a4ebfeef8a
Kestrel <kestrelg@kestrelscry.com>
2025-08-14 14:28:53 -0500

    Fix floor quaff message.

--- src/potion.c
+++ src/potion.c
@@ -587,8 +587,8 @@ dodrink(void)
             }
             ++drink_ok_extra;
         }
-        if (has_coating(u.ux, u.ux, COAT_POTION)
-            || has_coating(u.ux, u.ux, COAT_BLOOD)) {
+        if (has_coating(u.ux, u.uy, POT_WATER)
+            || has_coating(u.ux, u.uy, COAT_BLOOD)) {
             if (y_n("Drink liquid on the floor?") == 'y') {
                 pline("Do you know how many bugs there are on the floor?");
                 return ECMD_TIME;
