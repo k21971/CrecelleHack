k21971/EvilHack

Monster functionality: jumping and water walking. From SpliceHack - monsters can now use jumping and water walking boots. Monsters can also be flagged as jumpers.

src/mon.c
@@ -713,7 +713,7 @@ register struct monst *mtmp;
          * be handled here.  Swimmers are able to protect their stuff...
          */
         if (!is_clinger(mtmp->data) && !is_swimmer(mtmp->data)
-            && !amphibious(mtmp->data)) {
+            && !amphibious(mtmp->data)&& !can_wwalk(mtmp)) {
             /* like hero with teleport intrinsic or spell, teleport away
                if possible */
             if (can_teleport(mtmp->data) && !tele_restrict(mtmp)) {
@@ -1582,7 +1582,8 @@ long flag;
     nodiag = NODIAG(mdat - mons);
     wantpool = mdat->mlet == S_EEL;
     poolok = (is_flyer(mdat) || is_clinger(mdat)
-              || (is_swimmer(mdat) && !wantpool));
+              || (is_swimmer(mdat) && !wantpool)
+              || can_wwalk(mon));
     lavaok = (is_flyer(mdat) || is_clinger(mdat) || likes_lava(mdat));
     thrudoor = ((flag & (ALLOW_WALL | BUSTDOOR)) != 0L);
     poisongas_ok = ((nonliving(mdat) || is_vampshifter(mon)

k21971/CrecelleHack

src/mon.c
@@ -1105,7 +1105,7 @@ minliquid_core(struct monst *mtmp)
          * be handled here.  Swimmers are able to protect their stuff...
          */
         if ((waterwall || !is_clinger(mtmp->data))
-            && !cant_drown(mtmp->data)) {
+            && !cant_drown(mtmp->data) && !can_wwalk(mtmp)) {
             /* like hero with teleport intrinsic or spell, teleport away
                if possible */
             if (can_teleport(mtmp->data) && !tele_restrict(mtmp)) {
@@ -2246,7 +2246,8 @@ mfndpos(
     nodiag = NODIAG(mdat - mons);
     wantpool = (mdat->mlet == S_EEL);
     poolok = ((!Is_waterlevel(&u.uz) && m_in_air(mon))
-              || (is_swimmer(mdat) && !wantpool));
+              || (is_swimmer(mdat) && !wantpool)
+              || can_wwalk(mon));
     /* note: floating eye is the only is_floater() so this could be
        simplified, but then adding another floater would be error prone */
     lavaok = (m_in_air(mon) || likes_lava(mdat));