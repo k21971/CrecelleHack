copperwater/xNetHack

Add player sexuality choices
The option to play as something other than a straight character has been
a consistent request among some players. This implements a configurable
orientation option, allowing the character to be straight, gay, or
bisexual. This primarily affects which foocubi can seduce the character.

Proposals for how to handle this were many and varied; there was even a
patch versus 3.4.3 for it, but it worked by determining the orientation
of individual foocubi, not the player, and using their preferences in
interactions regardless of the player's. That was the exact opposite of
what people wanted. Instead, I largely followed SpliceHack's code,
making some changes.

The broad strokes: you can set your orientation via configuration file,
gay characters can interact with same-sex foocubi, straight characters
can interact with opposite-sex foocubi just like before, and bisexual
characters can interact with foocubi of either gender. (This presumes
that the foocubi themselves are all bisexual, which works fine.)

Changing gender will maintain your orientation respective to your new
gender; a gay male character that turns female will now be able to
interact with succubi, and vice versa. I suppose it would be possible to
devise an orientation system where you are only attracted to one gender
regardless of your current gender, but this seems like too much of a
stretch to be worth implementing. (It would also be strictly worse than
the current system, because you could no longer change gender intentionally
to use other-gendered foocubi.)

I deviated from SpliceHack in how it avoids making bisexuality
overpowered (without any modifications, bisexual characters would get to
have about twice as many foocubus interactions): where SpliceHack
generates half of all foocubi cancelled, I felt that made for fairly
weird flavor, so I made foocubi roll twice for getting a severe headache
after you encounter them instead.

Unlike SpliceHack, I declined to implement an asex

include/flag.h
@@ -31,6 +31,10 @@ struct flag {
     boolean explore; /* in exploration mode */
 #define discover flags.explore
     boolean female;
+    xchar orientation; /* index into orientations[] */
+#define ORIENT_STRAIGHT 0
+#define ORIENT_GAY 1
+#define ORIENT_BISEXUAL 2
     boolean friday13;        /* it's Friday the 13th */
     boolean help;            /* look in data file for info about stuff */
     boolean ignintr;         /* ignore interrupts */

include/you.h
@@ -241,10 +241,17 @@ struct Gender {
     const char *filecode; /* file code */
     short allow;          /* equivalent ROLE_ mask */
 };
+struct Orientation {
+    const char* adj;       /* gay/straight/bi */
+    const char* technical; /* homosexual/heterosexual/bisexual */
+    xchar mapping;         /* equivalent constant, e.g. ORIENT_STRAIGHT */
+};
 #define ROLE_GENDERS 2    /* number of permitted player genders
                              increment to 3 if you allow neuter roles */
+#define ROLE_ORIENTATIONS 3 /* number of defined player orientations */
 
 extern const struct Gender genders[]; /* table of available genders */
+extern const struct Orientation orientations[]; /* table of available genders */
 /* pronouns for the hero */
 #define uhe()      (genders[flags.female ? 1 : 0].he)
 #define uhim()     (genders[flags.female ? 1 : 0].him)

k21971/CrecelleHack

include/flag.h
@@ -34,6 +34,10 @@ struct flag {
     boolean explore;         /* in exploration mode (aka discover mode) */
 #define discover flags.explore
     boolean female;
+    uchar orientation; /* index into orientations[] */
+#define ORIENT_STRAIGHT 0
+#define ORIENT_GAY 1
+#define ORIENT_BISEXUAL 2
     boolean friday13;        /* it's Friday the 13th */
     boolean goldX;           /* for BUCX filtering, whether gold is X or U */
     boolean help;            /* look in data file for info about stuff */

include/you.h
@@ -347,10 +347,17 @@ struct Gender {
     const char *filecode; /* file code */
     short allow;          /* equivalent ROLE_ mask */
 };
+struct Orientation {
+    const char* adj;       /* gay/straight/bi */
+    const char* technical; /* homosexual/heterosexual/bisexual */
+    uchar mapping;         /* equivalent constant, e.g. ORIENT_STRAIGHT */
+};
 #define ROLE_GENDERS 2    /* number of permitted player genders
                              increment to 3 if you allow neuter roles */
+#define ROLE_ORIENTATIONS 3 /* number of defined player orientations */
 
 extern const struct Gender genders[]; /* table of available genders */
+extern const struct Orientation orientations[]; /* table of available genders */
 /* pronouns for the hero */
 #define uhe()      (genders[flags.female ? 1 : 0].he)
 #define uhim()     (genders[flags.female ? 1 : 0].him)